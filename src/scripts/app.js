f850c139411dcc667f2fb2fe9beae06042b496ef
Amadeo Bon <amadeosph@outlook.com>
2023-05-04 17:05:23 -0300

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const API_KEY = process.env.API_KEY;
+
+const submitIcon = document.querySelector("#submit-icon");
+const inputElement = document.querySelector("input");
+const imageSection = document.querySelector(".images-section");
+
+const getImages = async () => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: inputElement.value,
+      n: 4,
+      size: "1024x1024",
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      options,
+    );
+    const data = await response.json();
+    console.log(data);
+
+    data?.data.forEach((imageObject) => {
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("image-container");
+      const imageElement = document.createElement("img");
+      imageElement.setAttribute("src", imageObject.url);
+      imageContainer.append(imageElement);
+      imageSection.append(imageContainer);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+submitIcon.addEventListener("click", getImages);
